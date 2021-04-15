ba1540db60d3c54ab91fe6ff277f112716c34bcb
chekulhan <46518731+chekulhan@users.noreply.github.com>
2021-04-15 20:23:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pickle5 as pickle
 import pandas as pd
 
 # Use pickle to load in the pre-trained model
-#with open(f'model/bike_model_xgboost.pkl', 'rb') as f:
-#    model = pickle.load(f)
+with open(f'model/bike_model_xgboost.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 # Initialise the Flask app
 app = flask.Flask(__name__)
